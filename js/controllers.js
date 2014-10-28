8c0dd70e1d1098823b9effb3ed3c1d891e8a8e98
VladimirStrelok <alexander.glica@gmail.com>
2014-10-28 13:39:18 -0400

    fixed armor pen and reroll armor pen

--- js/controllers.js
+++ js/controllers.js
@@ -6,6 +6,7 @@ angular.module('MathHammer')
   $scope.shots=1;
   $scope.hit=false;
   $scope.wound=false;
+  $scope.armor_pen=false;
   $scope.melta=false;
   $scope.Math = Math;
 
@@ -31,5 +32,7 @@ angular.module('MathHammer')
         [5/6, 5/6, 5/6, 5/6, 5/6, 5/6, 5/6, 4/6, 3/6, 2/6],
         [5/6, 5/6, 5/6, 5/6, 5/6, 5/6, 5/6, 5/6, 4/6, 3/6],
     ];
+    $scope.melta_array = [1,.9722,.9166,.8333,.7222,.5833,.4166,.2777,.1666,.0833,.0277]
+
     console.log($scope.saves)
 }]);
